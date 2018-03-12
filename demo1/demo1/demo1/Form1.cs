f3c5c7e5ec5a652793a2dfe885c1336a150428fd
Leochens <18332518328@163.com>
2018-03-12 19:42:25 +0800

    创建了第一个窗体程序

---
+++ demo1/demo1/demo1/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace demo1
+{
+    public partial class Form1 : Form
+    {
+        private static System.Diagnostics.Process p;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string username = textBox1.Text;
+            string password = textBox2.Text;
+            if(username == "root" && password == "123456")
+            {
+                MessageBox.Show("Login Success！");
+                Form2 form2 = new Form2();
+                form2.Show();
+
+            }
+            else
+            {
+                MessageBox.Show("Login Failed！");
+            }
+            //textBox2.Text = username;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+    }
+}
